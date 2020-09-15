ce4c52b146d5906b5b5722f8192dfae28ccdb518
zadvornaya <yellowdragonjoy@gmail.com>
2020-09-15 16:59:02 +0400

    URL Building and HTTP Methods.

--- src/start.py
+++ src/start.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, request
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -6,40 +6,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return 'index'
 
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+def do_the_login():
+    return "do_the_login"
+
+
+def show_the_login_form():
+    return "show_the_login_form"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
 
 
 @app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % escape(username)
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
 
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return 'Subpath %s' % escape(subpath)
-
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-
-@app.route('/about')
-def about():
-    return 'The about page'
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
 
 
 if __name__ == "__main__":
